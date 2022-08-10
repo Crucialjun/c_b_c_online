c87a6144a5f01cb1c4597de1d0663965b0c2b22e
Crucialjun <nich.otieno@gmail.com>
2022-08-10 20:34:08 +0300

    add riverpod

--- lib/widgets/onboarding_pageview.dart
+++ lib/widgets/onboarding_pageview.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:c_b_c_online/models/onboarding_pageview_item.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +15,7 @@ class OnboardingPageView extends StatelessWidget {
     );
   }
 
-  Widget OnboardingPageViewHolder(
+  Widget onboardingPageViewHolder(
     OnboardingPageViewItem item,
   ) {
     return Column(
@@ -23,5 +25,13 @@ class OnboardingPageView extends StatelessWidget {
         Image.asset("imageUrl"),
       ],
     );
+  }
+
+  void loadItems(BuildContext context) async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString('assets/jsons/onboarding_items.json');
+    Iterable i = json.decode(data);
+    List<OnboardingPageViewItem> items =
+        i.map((model) => OnboardingPageViewItem.fromMap(model)).toList();
   }
 }
