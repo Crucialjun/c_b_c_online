4f03c8bd75eda98a0dbbe986c43881091939fe0c
Crucial Jun <nich.otieno@gmail.com>
2022-09-22 20:18:44 +0300

    add social buttons

--- lib/features/onboarding/screens/sign_up_screen.dart
+++ lib/features/onboarding/screens/sign_up_screen.dart
@@ -11,6 +11,67 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        toolbarHeight: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          //const SizedBox(height: 24),
+          const Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/pngs/facebook_icon.png",
+                        width: 32,
+                        height: 32,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const Text("Facebook")
+                    ],
+                  )),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/pngs/facebook_icon.png",
+                        width: 32,
+                        height: 32,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const Text("Facebook")
+                    ],
+                  ))
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:c_b_c_online/features/onboarding/screens/onboarding_screen.dart';
+import 'package:c_b_c_online/global_utils/error_screen.dart';
 import 'package:c_b_c_online/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,6 +22,8 @@ class MyApp extends StatelessWidget {
         fontFamily: GoogleFonts.lexendDeca().fontFamily,
       ),
       onGenerateRoute: ((settings) => generateRoute(settings)),
+      onUnknownRoute: ((settings) => MaterialPageRoute(
+          builder: (_) => const ErrorScreen(errorMsg: "Unknown Route"))),
       home: const OnboardingScreen(),
     );
   }
