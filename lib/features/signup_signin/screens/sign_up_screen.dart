e0c26dac034932d99fb3ff909dd7ffba8d48cf13
Crucialjun <nich.otieno@gmail.com>
2022-10-27 23:49:41 +0300

    add scroolview

--- lib/features/signup_signin/screens/sign_up_screen.dart
+++ lib/features/signup_signin/screens/sign_up_screen.dart
@@ -23,128 +23,130 @@ class _SignUpScreenState extends State<SignUpScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _signupFormKey,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            //const SizedBox(height: 24),
-            const Text(
-              "Sign Up",
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 48,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                SocialButton(socialName: "facebook"),
-                SocialButton(socialName: "google"),
-              ],
-            ),
-            const SizedBox(
-              height: 36,
-            ),
+          child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              //const SizedBox(height: 24),
+              const Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 48,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  SocialButton(socialName: "facebook"),
+                  SocialButton(socialName: "google"),
+                ],
+              ),
+              const SizedBox(
+                height: 36,
+              ),
 
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.black),
-                decoration: const TextFieldsDecoration(
-                    inputHintText: "Username", inputIcon: Icons.person),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const TextFieldsDecoration(
+                      inputHintText: "Username", inputIcon: Icons.person),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.black),
-                decoration: const TextFieldsDecoration(
-                    inputHintText: "Email", inputIcon: Icons.email_outlined),
+              const SizedBox(
+                height: 4,
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const TextFieldsDecoration(
+                      inputHintText: "Email", inputIcon: Icons.email_outlined),
+                ),
+              ),
 
-            const SizedBox(
-              height: 4,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.black),
-                decoration: const TextFieldsDecoration(
-                    inputHintText: "Password", inputIcon: Icons.lock),
+              const SizedBox(
+                height: 4,
               ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                style: const TextStyle(color: Colors.black),
-                decoration: const TextFieldsDecoration(
-                    inputHintText: "Confirm Password", inputIcon: Icons.lock),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const TextFieldsDecoration(
+                      inputHintText: "Password", inputIcon: Icons.lock),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(300, 50),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  onPressed: () {},
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text("Create Account"),
-                  )),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              children: const [
-                Expanded(
-                  child: Divider(
-                    color: Colors.black,
-                    thickness: 2.0,
+              const SizedBox(
+                height: 4,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const TextFieldsDecoration(
+                      inputHintText: "Confirm Password", inputIcon: Icons.lock),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(300, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Create Account"),
+                    )),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: const [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 2.0,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 4,
-                ),
-                Text(
-                  "Or",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 4,
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.black,
-                    thickness: 2.0,
+                  SizedBox(
+                    width: 4,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("Already have an account? "),
-                Text(
-                  "Log in",
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ],
-            )
-          ]),
+                  Text(
+                    "Or",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text("Already have an account? "),
+                  Text(
+                    "Log in",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              )
+            ]),
+          ),
         ),
       ),
     );
