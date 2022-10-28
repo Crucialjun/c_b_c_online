20049f82f8302112c3df2cee16ef3d7b2883f73f
nicho <nich.otieno@gmail.com>
2022-10-28 17:59:17 +0300

    add firebase

--- lib/firebase/firebase_authentication.dart
+++ lib/firebase/firebase_authentication.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirebaseAuthentication {
@@ -8,6 +9,18 @@ class FirebaseAuthentication {
 
   Stream checkUserAuthState() {
     return _firebaseAuth.userChanges();
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 }
 
